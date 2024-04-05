84071723186f44b145de03fe2bba3ad93d662412
Daniel88871 <druizl01@elpuig.xeill.net>
2024-04-05 15:45:21 +0200

    Prueba33

--- Codigo1/src/Main.java
+++ Codigo1/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Banana");
+        System.out.println("Hello world! buenas como estás???");
     }
 }
